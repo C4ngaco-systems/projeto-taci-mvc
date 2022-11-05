b09983f6d76b461b4e71c4f020b31999963ea901
pedro-ivo <peustratt@gmail.com>
2022-11-05 11:45:51 -0300

    refactor: reajustado form do visualizar-loja para form validation do boostrap

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -1,93 +1,95 @@
 function desabilitar(valor) {
-  var statusCnpj = document.getElementById("cnpj").disabled;
-  var statusNomeLoja = document.getElementById("nome-loja").disabled;
-  var statusEndereco = document.getElementById("endereco").disabled;
-  var statusTelefone = document.getElementById("telefone").disabled;
-  var statusResponsavel = document.getElementById("responsavel").disabled;
-  var statusCelular = document.getElementById("celular").disabled;
-  var statusEmail = document.getElementById("e-mail").disabled;
+  var statusCnpj = document.getElementById("validationCNPJ").disabled;
+  var statusNomeLoja = document.getElementById("validationStoreName").disabled;
+  var statusEndereco = document.getElementById("validationAdress").disabled;
+  var statusTelefone = document.getElementById("validationTel").disabled;
+  var statusResponsavel = document.getElementById("validationResp").disabled;
+  var statusCelular = document.getElementById("validationCelular").disabled;
+  var statusEmail = document.getElementById("validationEmail").disabled;
 
   if (valor == "sim" && !statusCnpj) {
-    document.getElementById("cnpj").disabled = true;
+    document.getElementById("validationCNPJ").disabled = true;
   } else {
-    document.getElementById("cnpj").disabled = false;
+    document.getElementById("validationCNPJ").disabled = false;
   }
   if (valor == "sim" && !statusNomeLoja) {
-    document.getElementById("nome-loja").disabled = true;
+    document.getElementById("validationStoreName").disabled = true;
   } else {
-    document.getElementById("nome-loja").disabled = false;
+    document.getElementById("validationStoreName").disabled = false;
   }
   if (valor == "sim" && !statusEndereco) {
-    document.getElementById("endereco").disabled = true;
+    document.getElementById("validationAdress").disabled = true;
   } else {
-    document.getElementById("endereco").disabled = false;
+    document.getElementById("validationAdress").disabled = false;
   }
   if (valor == "sim" && !statusTelefone) {
-    document.getElementById("telefone").disabled = true;
+    document.getElementById("validationTel").disabled = true;
   } else {
-    document.getElementById("telefone").disabled = false;
+    document.getElementById("validationTel").disabled = false;
   }
   if (valor == "sim" && !statusResponsavel) {
-    document.getElementById("responsavel").disabled = true;
+    document.getElementById("validationResp").disabled = true;
   } else {
-    document.getElementById("responsavel").disabled = false;
+    document.getElementById("validationResp").disabled = false;
   }
   if (valor == "sim" && !statusCelular) {
-    document.getElementById("celular").disabled = true;
+    document.getElementById("validationCelular").disabled = true;
   } else {
-    document.getElementById("celular").disabled = false;
+    document.getElementById("validationCelular").disabled = false;
   }
   if (valor == "sim" && !statusEmail) {
-    document.getElementById("e-mail").disabled = true;
+    document.getElementById("validationEmail").disabled = true;
   } else {
-    document.getElementById("e-mail").disabled = false;
+    document.getElementById("validationEmail").disabled = false;
   }
 }
 
 (function () {
-  'use strict'
+  "use strict";
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll(".needs-validation");
 
   // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-
+          event.preventDefault();
+          event.stopPropagation();
         }
 
-        form.classList.add('was-validated')
+        form.classList.add("was-validated");
 
-        if (form.checkValidity() == true){
+        if (form.checkValidity() == true) {
           var cnpj = document.getElementById("validationCNPJ");
           var nomeLoja = document.getElementById("validationStoreName");
           var endereco = document.getElementById("validationAdress");
           var cidade = document.getElementById("validationCity");
-          var estado = document.getElementById("validationState")
+          var estado = document.getElementById("validationState");
           var telefone = document.getElementById("validationTel");
           var cep = document.getElementById("validationCEP");
           var responsavel = document.getElementById("validationResp");
           var email = document.getElementById("validationEmail");
-        
+          var celular = document.getElementById("validationCelular");
+
           console.log(cnpj.value);
           console.log(nomeLoja.value);
           console.log(endereco.value);
           console.log(cidade.value);
           console.log(estado.value);
           console.log(telefone.value);
-          console.log(cep.value)
+          console.log(cep.value);
           console.log(responsavel.value);
           console.log(email.value);
-    
-          
-          window.location.href = './index.html'; 
-          alert("Loja cadastrada com sucesso!");  
+          console.log(celular);
+
+          window.location.href = "./index.html";
+          alert("Loja cadastrada com sucesso!");
         }
-        
-      }, false)
-    })
-})()
+      },
+      false
+    );
+  });
+})();
